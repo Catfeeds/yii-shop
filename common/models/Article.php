3910c3442aa65c8dd29ff3bc5dee5506009d6d3d
chenling <1245010531@qq.com>
2018-01-16 11:38:03 +0800

    test

--- common/models/Article.php
+++ common/models/Article.php
@@ -236,7 +236,7 @@ class Article extends \yii\db\ActiveRecord
         }
     }
     
-    public function getList($offset,$limit)
+    public static function getList($offset,$limit)
     {
     	return self::find()->select('[*]')->offset($offset)->limit($limit)->asArray()->all();
     }
