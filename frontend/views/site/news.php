f8aab11634e8ba7298b904fd0a5231b68ec6a89a
laber <1138696852@qq.com>
2018-01-17 18:18:53 +0800

    11111

--- frontend/views/site/news.php
+++ frontend/views/site/news.php
@@ -33,6 +33,35 @@
 			    mySwiper.swipeNext()
 			})
 		</script>
+		<script type="text/javascript">
+			$(function(){
+			    /*初始化*/
+			    var counter = 0; /*计数器*/
+			    var pageStart = 0; /*offset*/
+			    var pageSize = 6; /*size*/
+			    var isEnd = false;/*结束标志*/
+			    
+			    /*首次加载*/
+			    getData(pageStart, pageSize);
+			    
+			     
+				/*监听加载更多*/
+				$(window).scroll(function(){
+				  if(isEnd == true){
+				    return;
+				  }
+				 
+				  // 当滚动到最底部以上100像素时， 加载新内容
+				  // 核心代码
+				  if ($(document).height() - $(this).scrollTop() - $(this).height()<100){
+				    counter ++;
+				    pageStart = counter * pageSize;
+				       
+				    getData(pageStart, pageSize);
+				  }
+				});
+			});
+		</script>
 	</body>
 </html>
 
--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -12,3 +12,32 @@ $(function(){
 		$('.laber_hbg, .laber_enrol').hide();
 	})
 })
+function getData(offset,size){
+	$.ajax({
+		type: "GET",
+		url: "site/newslist?offset=0&size=2",
+		dataType: 'json',
+		success: function(reponse){
+			var data = reponse.articles;
+			console.log(data)
+			var sum = reponse.articles.length;
+			var result = '';
+			
+			//不够一页的数量
+			if(sum - offset < size){
+				size = sum - offset;
+			}
+			
+			for(var i=offset;i<(offset + size);i++){
+				result += '<div class="item"><a class="link_pic" href="#"><img src="'+data[i].thumb+'"></a><span>'+data[i].title+'</span><a class="link_p" href="#">'+data[i].text+'</a></div>'
+			}
+			$('#news_main').append(result);
+			if ( (offset + size) >= sum){
+			  isEnd = true;//没有更多了
+			}
+		},
+		error: function(xhr, type){
+	      alert('加载错误');
+	    }
+	});
+}
