2af392b6fca830bbde2e211a27dc4e2c3d09c6a3
laber <1138696852@qq.com>
2018-01-23 16:51:10 +0800

    11111

--- frontend/views/index/index.php
+++ frontend/views/index/index.php
@@ -66,10 +66,12 @@
 		    e.preventDefault()
 		    mySwiper.swipeNext()
 		  })
-		  var myPlayer = videojs('my-video');
-		  videojs("my-video").ready(function(){
-			var myPlayer = this;
-			myPlayer.play();
-		  });
 		</script>
 		<script type="text/javascript" src="js/video.min.js" ></script>
+		<script type="text/javascript">
+			var myPlayer = videojs('my-video');
+		    videojs("my-video").ready(function(){
+			  var myPlayer = this;
+			  myPlayer.play();
+		    });
+		</script>
