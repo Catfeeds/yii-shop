daa8c6ed45213f72e4081aeadef93c5af1f4e8e3
laber <1138696852@qq.com>
2018-01-30 18:25:13 +0800

    11111

--- frontend/views/layouts/footer.php
+++ frontend/views/layouts/footer.php
@@ -7,7 +7,7 @@
 	    			<p id="gfwx"><i></i>官方微信</p>
 	    			<em style="display: none;" class="ewm"><img src="/img/ewm.jpg"></em>
 	    		</div>
-	    		<b>亲临品鉴：</b><span>深圳市南山区蛇口街道岸湾六街鸿威海怡湾畔花<br/>园138号商铺</span>
+	    		<b>亲临品鉴：</b><span>深圳市南山区蛇口街道岸湾六街鸿威海怡湾畔花园138号商铺</span>
 	    		<div class="tel"><p>品茗预约电话：</p>0755 - 8827 8006</div>
     		</div>
     		<em>Copyright © 2016 reallytalent.cn        粤ICP备17036121号-1</em>
