0542cf26e387ddc53afad543a717df7a6810bf5b
laber <1138696852@qq.com>
2018-01-30 17:15:14 +0800

    11111

--- frontend/views/site/management.php
+++ frontend/views/site/management.php
@@ -1,6 +1,6 @@
 		<div class="container2">
 			<div class="laber_heart" data-aos="fade-down">
-				<img src="img/pic36.jpg" />
+				<img src="/img/pic36.jpg" />
 			</div>
 			<section class="laber_origin" data-aos="fade-down">
 				<div class="origin auto">
@@ -18,15 +18,15 @@
 			<section class="laber_wushanJJ">
 				<div class="wushanJJ auto">
 					<div class="wushanJJ_main">
-						<img style="margin-top: -60px;" data-aos="fade-left" src="img/pic39.jpg" />
-						<img data-aos="fade-right" class="tp" src="img/pic40.jpg" />
+						<img style="margin-top: -60px;" data-aos="fade-left" src="/img/pic39.jpg" />
+						<img data-aos="fade-right" class="tp" src="/img/pic40.jpg" />
 						<div class="wushanJJ_text">
 							<p>近几年，镇沅县委、政府提出将唱响“世界野生茶树王之乡”的茶树资源牌，挖掘镇沅茶文化，期望将老乌山古茶山打造成景迈山之后又一区域性品牌名茶山——正是这一举措让文榜茶业对“老乌山古茶树的保护性开发”提供了坚实的政策</p>
 							<p>
 								怀着对古茶树的保护、古树茶文化的传承的心愿，文榜将古茶树单株认养平台带入深圳，成为深圳首家采用这种保护性商业模式的茶企，旨在实现对古茶树纯粹滋味以及公益价值的双重探索，力保古茶树认养常态化，保护常态化！
 							</p>
 						</div>
-						<img data-aos="fade-right" src="img/pic41.jpg" />
+						<img data-aos="fade-right" src="/img/pic41.jpg" />
 					</div>
 				</div>
 			</section>
