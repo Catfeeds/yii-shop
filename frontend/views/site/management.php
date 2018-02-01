887c054317a7061158c4f6d764e66f47b4314e8d
laber <1138696852@qq.com>
2018-02-01 12:07:40 +0800

    11111

--- frontend/views/site/management.php
+++ frontend/views/site/management.php
@@ -1,16 +1,13 @@
 		<div class="container2">
-			<div class="laber_heart" data-aos="fade-down">
-				<img src="/img/pic36.jpg" />
-			</div>
 			<section class="laber_origin" data-aos="fade-down">
 				<div class="origin auto">
 					<div class="origin_main">
+						<img src="/img/pic36.jpg" />
 						<div class="origin_text">
 							<em>“我寻找好茶30年，八年前来到老乌山，看到这里的气候环境，就坚信，老乌山的古茶树一定能走出大山。”</em>
 							<p class="p1">—普洱茶叶世家陶氏家族后裔、茶叶专家陶林</p>
-							<p class="p2">老乌山为无量山的一条支脉，横跨镇沅县振太镇、按板镇、勐大镇等。老乌山平均海拔约2000米，年平均气候约14度，年降雨量1390mm-1502mm，森林植被优良，土壤优沃、含多种矿物质，</p>
-							<p class="p3">是生长优质茶叶的绝佳之地。</p>
-							<p class="p2">文榜以古茶树作为载体，连接一群热爱古茶树的消费者，通过良性的商业认养手段，共同参与完成“<b>云南普洱保护古茶树认养计划</b>”，让每一个爱茶人成为这个行业里践行<b>文化传承、保护自然遗产的“执行人”</b>。</p>
+							<p class="p2">老乌山为无量山的一条支脉，横跨镇沅县振太镇、按板镇、勐大镇等。老乌山平均海拔约2000米，年平均气候约14度，年降雨量1390mm-1502mm，森林植被优良，土壤优沃、含多种矿物质，<b>是生长优质茶叶的绝佳之地。</b></p>
+							<p class="p3">文榜以古茶树作为载体，连接一群热爱古茶树的消费者，通过良性的商业认养手段，共同参与完成“<b>云南普洱保护古茶树认养计划</b>”，让每一个爱茶人成为这个行业里践行<b>文化传承、保护自然遗产的“执行人”</b>。</p>
 						</div>
 					</div>
 				</div>
@@ -18,7 +15,7 @@
 			<section class="laber_wushanJJ">
 				<div class="wushanJJ auto">
 					<div class="wushanJJ_main">
-						<img style="margin-top: -60px;" data-aos="fade-left" src="/img/pic39.jpg" />
+						<img style="margin-top: -206px;" data-aos="fade-left" src="/img/pic39.jpg" />
 						<img data-aos="fade-right" class="tp" src="/img/pic40.jpg" />
 						<div class="wushanJJ_text">
 							<p>近几年，镇沅县委、政府提出将唱响“世界野生茶树王之乡”的茶树资源牌，挖掘镇沅茶文化，期望将老乌山古茶山打造成景迈山之后又一区域性品牌名茶山——正是这一举措让文榜茶业对“老乌山古茶树的保护性开发”提供了坚实的政策</p>
