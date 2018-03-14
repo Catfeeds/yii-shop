1b15f5ba5a2d7c5a52c526173a9231b510fe0382
chenling <1245010531@qq.com>
2018-03-14 18:17:13 +0800

    signup

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -85,7 +85,7 @@ use yii\helpers\Url;
 			},
 			methods: {
 				createdCode: function(){
-					code = ""; 
+					/*code = ""; 
 				    var codeLength = 4;//验证码的长度 
 				    var random = new Array(0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R', 
 				       'S','T','U','V','W','X','Y','Z');//随机数 
@@ -93,8 +93,8 @@ use yii\helpers\Url;
 				     //循环操作 
 				     var index = Math.floor(Math.random()*36);//取得随机数的索引（0~35） 
 				     code += random[index];//根据索引取得随机数加到code上 
-				    } 
-				    this.txmImg = '/site/captcha'+ '?' + code;//把code值赋给验证码 
+				    } */
+				    this.txmImg = '/site/captcha'+ '?' + Math.random();//把code值赋给验证码 
 				},
 				btnTxm: function(){    //点击刷新图片
 					this.createdCode();
