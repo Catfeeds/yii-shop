7bb497a39a4043225b3ba02c2da21a7df86cc763
laber <1138696852@qq.com>
2018-03-14 17:23:28 +0800

    11111

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -82,11 +82,20 @@ use yii\helpers\Url;
 				},
 				txmImg: '',
 				captcha:''
-			},
-			created: {
-			},		
+			},	
 			methods: {
-
+				btnTxm: function(){    //点击刷新图片
+					code = ""; 
+				    var codeLength = 4;//验证码的长度 
+				    var random = new Array(0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R', 
+				       'S','T','U','V','W','X','Y','Z');//随机数 
+				    for(var i = 0; i < codeLength; i++) {
+				     //循环操作 
+				     var index = Math.floor(Math.random()*36);//取得随机数的索引（0~35） 
+				     code += random[index];//根据索引取得随机数加到code上 
+				    } 
+				    this.txmImg = '/site/sendmsg' + code;//把code值赋给验证码 
+				},
 				//验证手机号
 				checkphone:function(){
 					var telreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/; 
