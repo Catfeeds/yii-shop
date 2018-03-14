48498610c95b32685a3db02a799f655c7eb393ec
laber <1138696852@qq.com>
2018-03-14 17:55:12 +0800

    11111

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -81,10 +81,7 @@ use yii\helpers\Url;
 				txmImg: ''
 			},
 			created: {
-				this.btnTxm();
-			},
-			methods: {
-				btnTxm: function(){    //点击刷新图片
+				createdCode: function(){
 					code = ""; 
 				    var codeLength = 4;//验证码的长度 
 				    var random = new Array(0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R', 
@@ -95,6 +92,11 @@ use yii\helpers\Url;
 				     code += random[index];//根据索引取得随机数加到code上 
 				    } 
 				    this.txmImg = '/site/sendmsg' + code;//把code值赋给验证码 
+				}
+			},
+			methods: {
+				btnTxm: function(){    //点击刷新图片
+					this.createdCode();
 				},
 				//验证手机号
 				checkphone:function(){
