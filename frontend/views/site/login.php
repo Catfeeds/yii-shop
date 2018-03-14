8e542de230eb0800053fd66cab0e070598433896
laber <1138696852@qq.com>
2018-03-14 14:25:35 +0800

    11111

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -44,7 +44,7 @@
 				<i @click="carQx"></i>
 				<span>登录成功！</span>
 				<div class="linkShop">
-					<a @click="btnOk" href="javascript:;">确定</a>
+					<a href="javascript:;">确定</a>
 				</div>
 			</div>			
 		</div>
@@ -74,7 +74,7 @@
 						if(this.logindata.mobile == ''){
 							this.logPhoneMsg = "手机号不能为空";
 							return false;
-						}else if(!telreg.test($('#user_name').val())){
+						}else if(!telreg.test(this.logindata.mobile)){
 							this.logPhoneMsg = "请输入有效号码";
 							return false;
 						}else {
