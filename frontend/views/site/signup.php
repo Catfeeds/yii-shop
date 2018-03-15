eaa804418108726e6ecdbbb91987daf3b4e5a198
laber <1138696852@qq.com>
2018-03-15 14:44:20 +0800

    11111

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -142,7 +142,7 @@ use yii\helpers\Url;
 				captchaTxt: function(){
 					var _this = this;
 					if(_this.captcha.length != 4){
-					  _this.disabled = true;
+					  _this.disabled = false;
 					  _this.msgtx = '请填写正确的图形验证码';
 					  return false;
 					}else {
