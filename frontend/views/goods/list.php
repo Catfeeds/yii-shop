534cad29cb233cd65bbad97dc2ee716850a9b2a7
laber <1138696852@qq.com>
2018-03-15 18:01:05 +0800

    11111

--- frontend/views/goods/list.php
+++ frontend/views/goods/list.php
@@ -25,7 +25,7 @@
 		<!-- 主体内容 end  -->
 	</body>
 	<script src="https://cdn.jsdelivr.net/npm/vue"></script>
-	<script type="text/javascript" src="js/axios.min.js" ></script>
+	<script type="text/javascript" src="/js/axios.min.js" ></script>
 	<script type="text/javascript">
 		var goods = new Vue({
 			el: '#goods',
