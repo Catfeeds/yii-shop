38a22996d33e1ea7260c57dc796fa21fa0ddf32a
laber <1138696852@qq.com>
2018-03-23 17:03:14 +0800

    11111

--- frontend/views/cart/index.php
+++ frontend/views/cart/index.php
@@ -158,20 +158,20 @@
             	},
             	carSc: function(){
             		// 通过indexof 来搜索当前选中的商品 找到索引 index
-            		var index = this.message.indexOf(this.curProduct);
+            		var _this = this;
             		$.ajax({
             			type:"POST",
             			url:"/cart/remove",
             			dataType: 'json',
-	                    data: {id: this.id},
+	                    data: {id: _this.id},
 	                    success: function(data){
 	                    	if(data.status == 0){
 	                    		console.log('成功');
-	                    		
+	                    		var index = _this.message.indexOf(_this.curProduct);
 		            // 获取索引 后删除元素 splice(index，1) 两个参数  第一个参数索引 第二个参数 删除个数
-				            this.message.splice(index ,1);// 从当前索引开始删，删除一个元素
-				            this.carShow = false;
-						    this.popupShow = false; // 删除后 弹框消失
+				            _this.message.splice(index ,1);// 从当前索引开始删，删除一个元素
+				            _this.carShow = false;
+						    _this.popupShow = false; // 删除后 弹框消失
 	                    	}else{
 	                    		console.log('失败');
 	                    	}
