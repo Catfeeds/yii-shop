a4bc3af6e23502b02c11203cb1baa5458add1a73
laber <1138696852@qq.com>
2018-03-24 11:14:23 +0800

    11111

--- frontend/views/cart/index.php
+++ frontend/views/cart/index.php
@@ -41,7 +41,7 @@
             								<i></i>
             							</em>
             						</div>            						
-            						<a class="dd2" class="d1" :href="shopUrl">
+            						<a class="dd2" class="d1" :href="item.shopUrl">
             							<img :src="imgurl + item.image">
             							<b>{{item.name}}</b>
             						</a>
@@ -124,7 +124,7 @@
 							for(var i in _this.message){
 								goodId = _this.message[i].id.$oid;
 								console.log(goodId)
-								_this.shopUrl = goodsUrl + '?id=' + goodId;
+								_this.message[i].shopUrl = goodsUrl + '?id=' + goodId;
 							}
 	                	}          	
 	                }
