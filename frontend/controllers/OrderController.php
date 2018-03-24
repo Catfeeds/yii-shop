27d5f24f9f85885cf83978baddf19c2d34570707
chenling <1245010531@qq.com>
2018-03-24 15:10:08 +0800

    test order confirm

--- frontend/controllers/OrderController.php
+++ frontend/controllers/OrderController.php
@@ -6,10 +6,18 @@ use Yii;
 use common\service\goods\GoodsService;
 class OrderController extends BaseController
 {	
+	protected  $selectTmp = 'selectcart';
 	
+	/**
+	* @desc 确认订单页面(此方法数据没有完全分离)
+	*/
 	public function actionIndex()
 	{
-		$goods = Yii::$app->request->post('goods');
+		$goods = Yii::$app->session->get($this->selectTmp);
+		if(!$goods)
+		{
+			return $this->redirect('index');
+		}
 		$data = GoodsService::getListByids($goods);
 		return $this->render('index',['goods' =>json_encode($data)]);
 	}
@@ -17,10 +25,16 @@ class OrderController extends BaseController
 	
 	
     /**
-    * @desc 订单确认
+    * @desc 订单确认(临时保存用户选择的数据)
     */
     public function actionConfirm()
     {	
+    	if(Yii::$app->request->isAjax)
+    	{
+	    	$goods = Yii::$app->request->post('goods');
+	    	Yii::$app->session->set($this->selectTmp,$goods);
+	    	return ['status' => 0,'msg' =>'ok'];
+    	}
     }
 
     
