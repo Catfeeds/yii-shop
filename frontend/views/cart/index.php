a7286919248f677582b58128bc68587bf3ea7700
laber <1138696852@qq.com>
2018-03-24 11:12:39 +0800

    11111

--- frontend/views/cart/index.php
+++ frontend/views/cart/index.php
@@ -41,7 +41,7 @@
             								<i></i>
             							</em>
             						</div>            						
-            						<a class="dd2" class="d1" :href="item.shopUrl">
+            						<a class="dd2" class="d1" :href="shopUrl">
             							<img :src="imgurl + item.image">
             							<b>{{item.name}}</b>
             						</a>
