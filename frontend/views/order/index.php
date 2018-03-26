462988f9c773a151b1b428a66f2d707a16f81866
chenling <1245010531@qq.com>
2018-03-26 09:04:38 +0800

    test

--- frontend/views/order/index.php
+++ frontend/views/order/index.php
@@ -110,5 +110,5 @@
 	<script type="text/javascript" src="/js/axios.min.js" ></script>
 	<script type="text/javascript">
     	var goods = '<?=$goods?>';
-    	console.log(goods.name);
+    	console.log(goods[0].name);
 	</script>
