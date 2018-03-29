4853fbe80271d55b9773677c23d24e84601c201a
laber <1138696852@qq.com>
2018-03-29 17:18:18 +0800

    11111

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -44,6 +44,7 @@ $this->beginPage() ?>
 			<div class="laber_top">
 				<div class="top_main auto">
 					<div class="login">
+						 <a class="loginCart" href="javascript:;">购物车</a>
 						  <?php if(!Yii::$app->user->isGuest):?>
 						  	<?php if(Yii::$app->user->identity->mobile):?>
 						  		<a href="#"><?=Yii::$app->user->identity->mobile?></a>
