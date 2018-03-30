09cd305790c067b4031c344444f00dfef4024127
chenling <1245010531@qq.com>
2018-03-30 14:30:15 +0800

    test

--- frontend/controllers/PayController.php
+++ frontend/controllers/PayController.php
@@ -100,6 +100,7 @@ class PayController extends BaseController
 			echo $result['err_code_des'];exit;
 		}
     	$url = $result["code_url"];
+    	ob_clean();
     	$url = \QRcode::png($url , $outfile = false, $level = QR_ECLEVEL_L, $size = 6, $margin = 2, $saveandprint=false );
     	return $this->render('weixin',['url'=>$url,'orderAmount' =>$order['order_amount']]);
     }
