cb95eb113218fc60dcf23cdc1594e6ca2333771d
li1138696852 <1138696852@qq.com>
2018-04-02 18:07:53 +0800

    11111

--- frontend/views/cart/index.php
+++ frontend/views/cart/index.php
@@ -84,7 +84,22 @@
 	<script type="text/javascript" src="/js/axios.min.js" ></script>
 	<script type="text/javascript">
 		 Vue.filter('formatMoney', function(val){
-		 	return val.toFixed(2); // 返回一个¥ 加上2位小数
+		 	val = val.toString().replace(/\$|\,/g,'');
+		    if(isNaN(val)) {
+		      val = "0";  
+		    } 
+		    let sign = (val == (val = Math.abs(val)));
+		    val = Math.floor(val*100+0.50000000001);
+		    let cents = val%100;
+		    val = Math.floor(val/100).toString();
+		    if(cents<10) {
+		       cents = "0" + cents
+		    }
+		    for (var i = 0; i < Math.floor((val.length-(1+i))/3); i++) {
+		        val = val.substring(0,val.length-(4*i+3))+',' + val.substring(val.length-(4*i+3));
+		    }
+		
+		    return (((sign)?'':'-') + val + '.' + cents);
 		 })
          var cartMain = new Vue({
          	el: '#cartmain',
