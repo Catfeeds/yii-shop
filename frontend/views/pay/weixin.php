60f510f66763102677fd71db9fd872f9de8d0725
li1138696852 <1138696852@qq.com>
2018-04-02 17:10:11 +0800

    11111

--- frontend/views/pay/weixin.php
+++ frontend/views/pay/weixin.php
@@ -44,7 +44,7 @@ use yii\helpers\Url;
         	methods: {
         		refreshData: function(){
         			$.ajax({
-			            url:'/user/paystatus',
+			            url:'/order/paystatus',
 			            type: 'GET',
 			            dataType: 'json',
 			            data: '',
