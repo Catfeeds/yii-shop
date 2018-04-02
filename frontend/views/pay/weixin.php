6b4b7a753dcd8137320e961aef97778b678fdeb6
li1138696852 <1138696852@qq.com>
2018-04-02 18:25:02 +0800

    11111

--- frontend/views/pay/weixin.php
+++ frontend/views/pay/weixin.php
@@ -40,6 +40,7 @@ use yii\helpers\Url;
         		orderId: id
         	},
         	created: function(){
+        		var self = this;
         		self.setinterval(this.refreshData(),1000)
         	},
         	methods: {
