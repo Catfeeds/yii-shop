8786df951cfb488f96276e08384a76a60156dd49
li1138696852 <1138696852@qq.com>
2018-04-03 15:06:40 +0800

    11111

--- frontend/views/order/list.php
+++ frontend/views/order/list.php
@@ -28,7 +28,7 @@
             				<ul>
             					<li v-for="(list, index) in ListData">
             						<div class="list_i od1" :class="{'lisBorder': list.goods_list.length > 1}">
-	            						<a v-for="item in list.goods_list" class="order_list" href="#">
+	            						<a v-for="item in list.goods_list" class="order_list" :href="goodUrl + '?id=' + item.goods_id">
 	            							<img :src="imgUrl + item.goods_image">
 	            							<b>{{ item.goods_name }}</b>
 	            							<p class="od2">{{ item.goods_num }}</p>
@@ -68,7 +68,8 @@
          		ListData: [],
          		goodList: [],
          		statusNum: 0,
-         		imgUrl: imgurl
+         		imgUrl: imgurl,
+         		goodUrl: goodsUrl
          	},
          	created: function(){
          		let _this = this;
@@ -87,25 +88,7 @@
                 			if(data.status == 0){
                 				console.log('数据获取成功');
                 				_this.ListData = data.data;            				
-                				console.log(_this.ListData);
-//              				for(var i in _this.ListData){
-//              					if(_this.ListData[i].order_status = 1){
-//	                					console.log(1)
-//	                					_this.statusNum = 1;
-//	                				}else if(_this.ListData[i].order_status = 2){
-//	                					console.log(2)
-//	                					_this.statusNum = 2;
-//	                				}else if(_this.ListData[i].order_status = 3){
-//	                					console.log(3)
-//	                					_this.statusNum = 3;
-//	                				}else if(_this.ListData[i].order_status =4){
-//	                					console.log(4)
-//	                					_this.statusNum = 4;
-//	                				}else if(_this.ListData[i].order_status ==5){
-//	                					console.log(5)
-//	                					_this.statusNum = 5;
-//	                				}
-//              				}               				
+                				console.log(_this.ListData);              				
                 			}
                 		}
                 	});
