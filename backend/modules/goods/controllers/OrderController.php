8c41c09ef324780b4ad77c4dc931ed428297a2a6
chenling <1245010531@qq.com>
2018-04-03 15:11:04 +0800

    order list

--- backend/modules/goods/controllers/OrderController.php
+++ backend/modules/goods/controllers/OrderController.php
@@ -27,25 +27,16 @@ class OrderController extends \yii\web\Controller
             'index' => [
                 'class' => IndexAction::className(),
                 'data' => function(){
-                    $query = Brand::find();
-                    $dataProvider = new ActiveDataProvider([
-                        'query' => $query
-                    ]);
+                    $searchModel = new Order();
+                    $dataProvider = $searchModel->search(yii::$app->getRequest()->getQueryParams());
                     return [
                         'dataProvider' => $dataProvider,
+                        'searchModel' => $searchModel
                     ];
                 }
             ],
-            'create' => [
-                'class' => CreateAction::className(),
-                'modelClass' => Brand::className(),
-            ],
             'update' => [
                 'class' => UpdateAction::className(),
-                'modelClass' => Brand::className(),
-            ],
-            'delete' => [
-                'class' => DeleteAction::className(),
                 'modelClass' => Brand::className(),
             ],
         ];
