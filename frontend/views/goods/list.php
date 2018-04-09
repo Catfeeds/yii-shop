21884b6ce282695eeef75f9876564c7fe814a391
li1138696852 <1138696852@qq.com>
2018-04-09 16:08:32 +0800

    11111

--- frontend/views/goods/list.php
+++ frontend/views/goods/list.php
@@ -19,6 +19,17 @@ use yii\helpers\Url;
             					</a>
             				</li>
             			</ul>
+            			<div class="pagMain">
+							<div class="itemPage" v-show="current != 1" @click="current-- && goto(current)">
+								<a href="#">上一页</a>
+							</div>
+							<div class="itemPage" v-for="index in pages" @click="goto(index)" :class="{'active':current == index}" :key="index">
+								<a href="#">{{index}}</a>
+							</div>
+							<div class="itemPage" v-show="allpage != current && allpage != 0 " @click="current++ && goto(current++)">
+								<a href="#">下一页</a>
+							</div>
+						</ul>
             		</div>
             	</div>
             </section>
@@ -34,41 +45,120 @@ use yii\helpers\Url;
 		var goods = new Vue({
 			el: '#goods',
 			data: {
+				size: 4,
+				current: 1,  //当前页
+				showItem: 5, // 显示条目
+				allpage:4, //总页数
 				aLis: [],
 				id:'',
 				thisUrl:'',
 				noneCar: true,
 				goooList: true
 			},
+			computed: {
+				pages: function() {
+					var pag = [];
+					if(this.current < this.showItem) { //如果当前的激活的项 小于要显示的条数
+						//总页数和要显示的条数那个大就显示多少条
+						var i = Math.min(this.showItem, this.allpage);
+						while(i) {
+							pag.unshift(i--);
+						}
+					} else { //当前页数大于显示页数了
+						var middle = this.current - Math.floor(this.showItem / 2), //从哪里开始
+							i = this.showItem;
+						if(middle > (this.allpage - this.showItem)) {
+							middle = (this.allpage - this.showItem) + 1
+						}
+						while(i--) {
+							pag.push(middle++);
+						}
+					}
+					return pag
+				},
+				data: function(){
+					var _this = this;
+					$.ajax({
+		                url: '/goods/getlist?page=' + _this.current,
+		                type: 'POST',
+		                dataType: 'json',
+		                data: '',
+		                success: function(data) {	                 	
+			                if(data.status =='0')
+				            {
+				            	_this.aLis = data.data;
+				            	
+				            	if(_this.aLis.length != 0){
+					            	_this.goooList = true;
+					            	_this.noneCar = false;
+					            }else{
+					            	_this.goooList = false;
+					            	_this.noneCar = true;
+					            }
+				                var list = data.data
+			                 	for(var i in list){
+									_this.id = list[i]._id.$oid;
+									list[i].thisUrl = goodsUrl + '?id=' + _this.id;
+								}	                 	
+					        }else{
+					        	alert('页面信息错误');
+					        }
+		                }
+		            })	
+//					var main = [];
+//					var length = (_this.current - 1) * _this.size;
+//					console.log(length)
+//					for(var i=length;i < length +  _this.size;i++){
+//						console.log(_this.list[i]);
+//						if(!_this.list[i]){
+//							console.log('没数据了');
+//						}else{
+//							main.push(_this.list[i]);
+//						}
+//					}
+//					return main;
+				}
+			},
 			created: function(){
-				var _this = this;										
-				$.ajax({
-	                url: '/goods/getlist',
-	                type: 'POST',
-	                dataType: 'json',
-	                data: '',
-	                success: function(data) {	                 	
-		                if(data.status =='0')
-			            {
-			            	_this.aLis = data.data;
-			            	if(_this.aLis.length != 0){
-				            	_this.goooList = true;
-				            	_this.noneCar = false;
-				            }else{
-				            	_this.goooList = false;
-				            	_this.noneCar = true;
-				            }
-			                var list = data.data
-		                 	for(var i in list){
-								_this.id = list[i]._id.$oid;
-								list[i].thisUrl = goodsUrl + '?id=' + _this.id;
-							}	                 	
-				        }else{
-				        	alert('页面信息错误');
-				        }
-	                }
-	            })
-	            
+				this.data;
+			},
+//			created: function(){
+//				var _this = this;										
+//				$.ajax({
+//	                url: '/goods/getlist',
+//	                type: 'POST',
+//	                dataType: 'json',
+//	                data: '',
+//	                success: function(data) {	                 	
+//		                if(data.status =='0')
+//			            {
+//			            	_this.aLis = data.data;
+//			            	
+//			            	if(_this.aLis.length != 0){
+//				            	_this.goooList = true;
+//				            	_this.noneCar = false;
+//				            }else{
+//				            	_this.goooList = false;
+//				            	_this.noneCar = true;
+//				            }
+//			                var list = data.data
+//		                 	for(var i in list){
+//								_this.id = list[i]._id.$oid;
+//								list[i].thisUrl = goodsUrl + '?id=' + _this.id;
+//							}	                 	
+//				        }else{
+//				        	alert('页面信息错误');
+//				        }
+//	                }
+//	            })	            
+//			},
+			meathods: {
+				goto: function(index) {
+					if(index == this.current) return;
+					this.current = index;
+					//这里可以发送ajax请求
+					this.data;
+				}
 			}
 		})
 	</script>
