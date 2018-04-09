bb165b6dbea133422c476215cbdce820bab192df
chenling <1245010531@qq.com>
2018-04-09 17:07:56 +0800

    sms test

--- common/config/params.php
+++ common/config/params.php
@@ -39,7 +39,7 @@ return [
     'aliyun_sms' =>[//阿里短信
     	'accessKeyId' =>'LTAIUloKarS76KjI',
     	'accessKeySecret' =>'D26FYz9pDKazgi4SBwDQtaxGQLwVGY',
-    	'SignName' =>'深圳文榜茶叶',
+    	'SignName' =>'深圳文榜',
     	'TemplateCode' =>'SMS_130745225'
     ]
 ];
