81a0db6cf964ee14602dff4434614d003e0d2118
li1138696852 <1138696852@qq.com>
2018-04-23 17:55:07 +0800

    11111

--- frontend/views/address/index.php
+++ frontend/views/address/index.php
@@ -381,7 +381,7 @@
 			            data: {id:item.id},
 			            success: function(data) {
 			                if(data.status == 0){
-			                   console.log('data.data');                
+			                   console.log(data.data);                
 			                }else{
 			                	console.log('shibai')
 			                }              	
