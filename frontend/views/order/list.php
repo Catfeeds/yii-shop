214e950d08ddc44a7d9e4971f5ca1347ead70c99
li1138696852 <1138696852@qq.com>
2018-04-23 14:29:20 +0800

    11111

--- frontend/views/order/list.php
+++ frontend/views/order/list.php
@@ -51,7 +51,7 @@
 	            						<div class="btn_cz">
 	            							<button @click="orders_qx(list)" type="button" v-show="list.order_status == 1" class="orders_qx">取消订单</button>
 	            							<button @click="orders_zf(list)" type="button" v-show="list.order_status == 1" class="orders_zf">立即支付</button>
-	            							<a :href="newUrl" @click="orders_gz(list)" target='_blank' v-show="list.order_status == 3" class="orders_zf">物流跟踪</button>
+	            							<a :href="newUrl" target='_blank' @click="orders_gz(list)"  v-show="list.order_status == 3" class="orders_zf">物流跟踪</button>
 	            							<button type="button" v-show="list.order_status == 5" class="orders_zf">交易完成</a>
 	            						</div>
             						
