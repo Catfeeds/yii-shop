64b9f53c90b0bba0d7d9ad19ba9cd748f2f0e76b
li1138696852 <1138696852@qq.com>
2018-04-25 18:06:34 +0800

    11111

--- frontend/views/cart/index.php
+++ frontend/views/cart/index.php
@@ -2,7 +2,6 @@
             <section class="laber_shopUser">
             	<div class="shopUser auto clearfix">
             		<div class="shopUser_main">
-            			<p>您好，15038384758</p>
             			<ul>
             				<li><a href="/user/updatepassword">密码管理</a></li>
             				<li class="on"><a href="/cart/index">购物车</a></li>
