c1f70d48cbbc896311c87641f117ac0b31ddede9
li1138696852 <1138696852@qq.com>
2018-04-25 09:59:20 +0800

    11111

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -34,7 +34,7 @@ use yii\helpers\Url;
 									<strong class="stro1">{{ msgpas }}</strong>
 								</div>
 								<a href="/site/requestpasswordreset" class="forget">忘记密码?</a>
-								<button type="button" @keyup.native="register" :disabled="disabled2" class="immediately" :class="{active1: isactive1, active2: isactive2}">立即注册</button>
+								<button type="button" @keyup.enter.native="register" :disabled="disabled2" class="immediately" :class="{active1: isactive1, active2: isactive2}">立即注册</button>
 								<p>若您已有账号，可点击这里<a href="/site/login">登录</a></p>
 								<div class="qt">
 									<b class="b1"></b>
